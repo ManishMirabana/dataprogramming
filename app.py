664a0d6d55e9ad89fb38390d4d8113af44f8a5db
200518197 <200518197@student.georgianc.on.ca>
2022-08-11 13:03:34 -0800

    ini

---
+++ app.py
@@ -0,0 +1,91 @@
+from flask import Flask, render_template, json, template_rendered, Response, Request
+from pymongo import MongoClient, DESCENDING
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+import requests
+import pytz
+import time
+import os
+import sys
+import json
+import schedule
+#from flask_apscheduler import APScheduler
+ 
+
+load_dotenv()
+
+app = Flask(__name__)
+
+#scheduler = APScheduler()
+
+def covidData_serializer(item) -> dict:
+    return {
+        "id":str(item["_id"]),
+        "data":item["data"]
+    }
+
+def covidDatas_serializer(items) -> list:
+    return [covidData_serializer(item) for item in items]
+
+
+conn = MongoClient("mongodb+srv://admin:iW70CzDypD5z8Kr5@api.uq5l2w9.mongodb.net/Covid_Data?retryWrites=true&w=majority")
+db = conn['Covid_Data']
+collection_name = db["Covid_Data"]
+
+@app.route("/health", methods=["GET"])
+def getHealth():
+    data = covidDatas_serializer(db.Cases.find())
+    return Response(
+            response= json.dumps(data),status=500,mimetype="application/json"
+        )
+
+@app.route("/",methods=["GET"])
+def getCovidData():
+    try:
+        data = covidDatas_serializer(db.Cases.find())
+        print("from html")
+        return render_template('index.html', data1 = Response(
+            response= json.dumps(data),status=500,mimetype="application/json"
+        )
+)
+    except Exception as ex:
+        print(ex)
+        return Response(
+            response= json.dumps({"msg":"Failed to retrive Data"}),status=500,mimetype="application/json"
+        )
+        
+
+url = "https://covid-19-tracking.p.rapidapi.com/v1"
+headers = {
+    "X-RapidAPI-Key": "70ffb4e16emsh9f5eb9947f6a419p1a2ed3jsne4d2d72523ad",
+    "X-RapidAPI-Host": "covid-19-tracking.p.rapidapi.com"
+}
+
+countries = ["USA","Spain","Italy","UK","India"]
+yesterday = datetime.now() - timedelta(1)
+currentDate = str(datetime.strftime(yesterday, '%Y-%m-%d'))
+
+def data_load():
+    for item in countries:
+        print(item)
+        querystring = {"country_text":item,"date":currentDate}
+        r = requests.request("GET", url, headers=headers, params=querystring)
+        if r.status_code == 200:
+            data = r.json()
+            collection_name.insert_one(data)
+
+#schedule.every().day.at("14:03").do(data_load)
+
+#def job():
+    #print('hello')
+
+#schedule.every(30).seconds.do(job)
+
+# while 1:
+#     schedule.run_pending()
+#     time.sleep(1)
+
+
+if __name__ == "__main__":
+    #scheduler.add_job(id="test",func = job, trigger = 'interval', seconds = 5)
+    app.run(debug=True)
